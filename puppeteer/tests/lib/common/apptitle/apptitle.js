bb1cff8108bd32f837adef303fabbf02f57d5bf1
Andrey Muminov <andrey.muminov@onlyoffice.com>
2025-09-10 20:04:23 +0300

    Update apptitle.js

--- puppeteer/tests/lib/common/apptitle/apptitle.js
+++ puppeteer/tests/lib/common/apptitle/apptitle.js
@@ -1,19 +1,19 @@
 const { AppTitle } = require("lib");
 
-// // Create a new DOCX file for testing
-// Tester.createFile("docx");
+// Create a new DOCX file for testing
+Tester.createFile("docx");
 
-// // Input sample text into the document
-// Tester.input("Lorem Ipsum is simply dummy text of the printing and typesetting industry.");
+// Input sample text into the document
+Tester.input("Lorem Ipsum is simply dummy text of the printing and typesetting industry.");
 
-// // Click undo button
-// AppTitle.clickUndoButton();
+// Click undo button
+AppTitle.clickUndoButton();
 
-// // Click redo button
-// AppTitle.clickRedoButton();
+// Click redo button
+AppTitle.clickRedoButton();
 
-// // Click save button
-// AppTitle.clickSaveButton();
+// Click save button
+AppTitle.clickSaveButton();
 
 // Close the test
 Tester.close();
