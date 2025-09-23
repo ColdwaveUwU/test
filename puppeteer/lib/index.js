9c2578ec5cc50b4fe66d4e9dd173a2c81e82b79a
Andrey Muminov <andrey.muminov@onlyoffice.com>
2025-09-23 20:51:07 +0300

    1

--- puppeteer/lib/index.js
+++ puppeteer/lib/index.js
@@ -1,9 +1,34 @@
+const baseModules = require("../module");
+const providers = require("../providers");
 const { createProxy, collectClasses } = require("../engine/script/js");
-const modules = require("../module/editor");
-const classMap = collectClasses(modules);
+
+const providerModules = providers[globalThis.providerName]?.modules ?? {};
+const baseClassMap = collectClasses(baseModules);
+const providerClassMapRaw = collectClasses(providerModules);
+
+const classMap = { ...baseClassMap };
+
+function getParentClassName(cls) {
+    const parent = Object.getPrototypeOf(cls.prototype)?.constructor;
+    if (parent && parent !== Object) {
+        return parent.name;
+    }
+    return null;
+}
+
+for (const [provName, ProvClass] of Object.entries(providerClassMapRaw)) {
+    const parentName = getParentClassName(ProvClass);
+
+    if (parentName && baseClassMap[parentName]) {
+        classMap[parentName] = ProvClass;
+    } else {
+        classMap[provName] = ProvClass;
+    }
+}
+
 const instances = {};
 for (const name in classMap) {
-    instances[name] = createProxy(new classMap[name]());
+    instances[name] = createProxy(Object.assign(new classMap[name](), { className: name }));
 }
 
 module.exports = instances;
---
+++ puppeteer/module/index.js
@@ -0,0 +1,8 @@
+const Collab = require("./collab");
+const Common = require("./common");
+const Editor = require("./editor");
+module.exports = {
+    Collab,
+    Common,
+    Editor,
+};
